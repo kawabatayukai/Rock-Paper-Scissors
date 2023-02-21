33b131bc12a29637eb05e90532185913c2d4a430
kawabatayukai <107020988+kawabatayukai@users.noreply.github.com>
2023-02-21 14:51:39 +0900

    足場の設定項目追加
    画像
    色

--- Rock-Paper-Scissors/Floor.h
+++ Rock-Paper-Scissors/Floor.h
@@ -8,6 +8,12 @@ public:
 	//コンストラクタ  
 	Floor(int x = 0, int y = 700, int w = 1280, int h = 20);
 
+	//コンストラクタ  （色を指定）
+	Floor(int x, int y , int w, int h, int color);
+
+	//コンストラクタ  （画像の使用）
+	Floor(const char* image_handle = nullptr, int x = 0, int y = 700, int w = 1280, int h = 20);
+
 	void Draw() const;                //描画
 
 	int GetX() const { return x; }    //ｘ座標取得
@@ -21,4 +27,7 @@ private:
 	int y;
 	int w;
 	int h;
+
+	int image = 0;          //画像
+	int color = 0xffffff;   //色
 };
