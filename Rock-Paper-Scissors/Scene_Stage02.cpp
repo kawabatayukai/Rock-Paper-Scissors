193d246332172946961a8ee70ada6ad76fce3630
yonahara127 <107020969+yonahara127@users.noreply.github.com>
2023-02-24 09:12:52 +0900

    背景画像途中

--- Rock-Paper-Scissors/Scene_Stage02.cpp
+++ Rock-Paper-Scissors/Scene_Stage02.cpp
@@ -30,6 +30,8 @@ Scene_Stage02::Scene_Stage02(const Player* player)
 	obj_floor[1] = new Floor(0, 0, 20, 1720);           //壁（左）
 	obj_floor[2] = new Floor(1260, 0, 20, 1720);           //壁（右）
 	obj_floor[3] = new Floor(1000, 100, 120, 50);      //足場
+
+	image_back = LoadGraph("images/stage07/back02.png");
 }
 
 //デストラクタ
@@ -223,6 +225,9 @@ void Scene_Stage02::Update()
 //描画
 void Scene_Stage02::Draw() const
 {
+	//背景
+	DrawGraph(0, 0, image_back, TRUE);
+
 	//接触じゃんけんでない時
 	if (janken_flag == false)
 	{
