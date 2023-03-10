261922310f80479e461af1b4681a21b6f7e6b9f3
biseryuki <107020902+biseryuki@users.noreply.github.com>
2023-03-10 14:52:03 +0900

    プレイヤーめがけてとぶ処理の追加（ジグザグ）

--- Rock-Paper-Scissors/Jangeki_Zigzag.cpp
+++ Rock-Paper-Scissors/Jangeki_Zigzag.cpp
@@ -1,13 +1,16 @@
 #include "Jangeki_Zigzag.h"
 #include "DxLib.h"
+#include "Scene_Stage05.h"
+#include <math.h>
 
 
 //コンストラクタ
-Jangeki_Zigzag::Jangeki_Zigzag(float x, float y, float r, float speed, Jan_Type type)
-	:Jangeki_Base(x, y, r, speed, type)
+Jangeki_Zigzag::Jangeki_Zigzag(float x, float y, float r, float speed, Jan_Type type, float p_x, float p_y)
+	:Jangeki_Base(x, y, r, speed, type), player_x(p_x), player_y(p_y)
 {
 	zigzag = 0.f;
 	flg = FALSE;
+	rad = atan2f(p_y - y, p_x - x);
 }
 
 //デストラクタ
@@ -19,10 +22,14 @@ Jangeki_Zigzag::~Jangeki_Zigzag()
 //アップデート
 void Jangeki_Zigzag::Update()
 {
+
+	x += cosf(rad) * speed;
+	y += sinf(rad) * speed;
+
 	if (flg == FALSE)
 	{
 		zigzag += 3.f;
-		
+
 	}
 	else if (flg == TRUE)
 	{
@@ -41,5 +48,5 @@ void Jangeki_Zigzag::Update()
 
 	y += zigzag;
 
-	x -= 3.f;
+
 }
--- Rock-Paper-Scissors/Jangeki_Zigzag.h
+++ Rock-Paper-Scissors/Jangeki_Zigzag.h
@@ -7,10 +7,13 @@ class Jangeki_Zigzag : public Jangeki_Base
 private:
 	float zigzag;
 	bool flg;
+	float player_x;
+	float player_y;
+	float rad;
 
 public:
 	//コンストラクタ
-	Jangeki_Zigzag(float x, float y, float r, float speed, Jan_Type type);
+	Jangeki_Zigzag(float x, float y, float r, float speed, Jan_Type type,float p_x, float p_y);
 	~Jangeki_Zigzag();
 
 	virtual void Update()override;
