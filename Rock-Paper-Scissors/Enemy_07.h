e0a85d7e40c35852d6a2f5e0953c02e0e1df1190
kawabatayukai <107020988+kawabatayukai@users.noreply.github.com>
2023-03-20 10:28:09 +0900

    属性変更関数
    Enemyクラス外で属性を変更したいときはSetType()を呼んでください

--- Rock-Paper-Scissors/Enemy_07.h
+++ Rock-Paper-Scissors/Enemy_07.h
@@ -28,6 +28,8 @@ public:
 
 	void Move_Pattern();             //行動パターンに沿った行動
 
+	void Jan_360degrees(int count, float rad, float speed, Jan_Type type);  //360度発射（必殺）
+
 private:
 	/********************   ジャンプ関係   ********************/
 
