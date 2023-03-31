6654284cf836702e0a245a52e5908619b8e70b50
yozayosihiro <107020844+yozayosihiro@users.noreply.github.com>
2023-03-31 14:32:37 +0900

    敵の動き・難易度調整

--- Rock-Paper-Scissors/Enemy_10.cpp
+++ Rock-Paper-Scissors/Enemy_10.cpp
@@ -3,6 +3,7 @@
 #include"Player.h"
 #include"Jangeki_Base.h"
 #include"Jangeki_Homing.h"
+#include"Jangeki_Coming.h"
 
 //コンストラクタ　   基底クラスのコンストラクタを呼ぶ　　　　 ｘ　ｙ　幅　　　高さ    属性
 Enemy_10::Enemy_10(float x, float y, Jan_Type type) : EnemyBase(x, y, 100.0f, 100.0f, type)
@@ -11,7 +12,8 @@ Enemy_10::Enemy_10(float x, float y, Jan_Type type) : EnemyBase(x, y, 100.0f, 10
 	dir = 1;
 	hp = 100;
 
-	image = LoadGraph("images/tyokitest.png");
+	//image = LoadGraph("images/tyokitest.png");
+	if (LoadDivGraph("images/ワンパンマンALL画像.png", 10, 5, 2, 100, 100, image) == -1);
 
 	Init_Jangeki();       //じゃん撃を用意
 
@@ -35,6 +37,7 @@ void  Enemy_10::Move()
 		if (x > 120) //左へ移動
 		{
 			x--;
+			dir = static_cast<int>(DIRECTION::LEFT);   //向きを設定（左）
 
 			if (land_flg == true && x < 990 && x > 200) //ジャンプ
 			{
@@ -55,12 +58,14 @@ void  Enemy_10::Move()
 		{
 			switchMove = 1; //次の処理へ
 		}
+		enemyChange_Image = 2; //switch文の割り当て番号
 		break;
 
 	case 1:
 		if (x < 1100) //右へ移動
 		{
 			x++;
+			dir = static_cast<int>(DIRECTION::RIGHT);  //向きを設定（右）
 
 			if (land_flg == true && x > 200 && x < 1000) //ジャンプ
 			{
@@ -81,8 +86,12 @@ void  Enemy_10::Move()
 		{
 			switchMove = 0; //次の処理へ
 		}
+		enemyChange_Image = 1; //switch文の割り当て番号
 		break;
 	}
+
+	//画像の選択変更
+	EnemySwitch();
 
 	/*敵直接の属性変化*/
 	//e_type = Jan_Type::PAPER;
@@ -151,7 +160,8 @@ void Enemy_10::Update()
 void Enemy_10::Draw() const
 {
 	//中心から描画
-	DrawRotaGraphF(x, y, 1, 0, image, TRUE);
+	DrawRotaGraphF(x, y, 1, 0, image[enemy_Image], TRUE);
+	//DrawRotaGraphF(x, y, 1, 0, image, TRUE);
 
 	//じゃん撃描画
 	Draw_Jangeki();
@@ -162,6 +172,205 @@ void Enemy_10::Draw() const
 	DrawFormatString(500, 200, 0xffffffff, "%f", x);
 
 }
+
+/*画像の変更取得*/
+void  Enemy_10::EnemySwitch()
+{
+	/*プレイヤーの画像変更*/
+	switch (enemyChange_Image)
+	{
+		/*左右移動*/
+	case 1: //右移動
+	   /*     右向き            接地   */
+		if (dir == static_cast<int>(DIRECTION::RIGHT) && land_flg == true)
+		{
+			enemyGetMove = 1;      //画像を取得
+			enemyCount++;          //フレームカウントを増やす
+			EnemyChangeMoveimg();  //画像変更へ
+
+			//pCount = 0;             //動いてないとき用
+			//enemyChange_Image = 0; //動いてないとき用
+			break;
+		}       /*  右向き         非接地     */
+		else if (dir == static_cast<int>(DIRECTION::RIGHT) && land_flg == false)
+		{
+			enemy_Image = 4;     //画像を取得
+			break;
+		}
+
+	case 2: //左移動の時
+	   /*     左向き            接地  */
+		if (dir == static_cast<int>(DIRECTION::LEFT) && land_flg == true)
+		{
+			enemyGetMove = 6;      //画像を取得
+			enemyCount++;          //フレームカウントを増やす
+			EnemyChangeMoveimg();  //画像変更へ
+
+			//pCount = 5;             //動いてないとき用
+			//enemyChange_Image = 0; //動いてないとき用
+			break;
+		}       /*  左向き         非接地     */
+		else if (dir == static_cast<int>(DIRECTION::LEFT) && land_flg == false)
+		{
+			enemy_Image = 9;     //画像を取得
+			break;
+		}
+
+		//case 0: //止まっている
+
+		//   /*     左向き            接地                                           押されてない   */
+		//	if (dir == 0 && land_flg == true && KeyManager::OnPadPressed(PAD_INPUT_RIGHT) == FALSE)
+		//	{
+		//		pCount = 0;  //動いてないとき用
+		//	}
+		//	/*     右向き            接地                                           押されてない  */
+		//	if (dir == 1 && land_flg == true && KeyManager::OnPadPressed(PAD_INPUT_LEFT) == FALSE)
+		//	{
+		//		pCount = 5;  //動いてないとき用
+		//	}
+		//	enemyGetMove = pCount; //最後に動いた時の画像
+		//	enemyCount++;          //フレームのカウント
+		//	EnemyChangeMoveimg();  //画像変更へ
+		//	break;
+
+	default:
+		break;
+	}
+}
+
+/*画像の動作変更*/
+void  Enemy_10::EnemyChangeMoveimg()
+{
+	static int std = 0; //画像の基準
+
+	/*画像の保持*/
+	switch (enemyGetMove)
+	{
+		/*右移動*/
+	case 1: //1の画像
+		if (enemy_Image != 2 && enemy_Image != 3)
+		{
+			enemy_Image = 1;
+			std = 1; //セット処理
+		}
+		break;
+	case 2: //2の画像
+		if (enemy_Image != 1 && enemy_Image != 3)
+		{
+			enemy_Image = 2;
+			std = 2; //セット処理
+		}
+		break;
+	case 3: //3の画像
+		if (enemy_Image != 1 && enemy_Image != 2)
+		{
+			enemy_Image = 3;
+			std = 3; //セット処理
+		}
+		break;
+
+		/*左移動*/
+	case 6: //6の画像
+		if (enemy_Image != 7 && enemy_Image != 8)
+		{
+			enemy_Image = 6;
+			std = 6; //セット処理
+		}
+		break;
+	case 7: //7の画像
+		if (enemy_Image != 6 && enemy_Image != 8)
+		{
+			enemy_Image = 7;
+			std = 7; //セット処理
+		}
+		break;
+	case 8: //8の画像
+		if (enemy_Image != 6 && enemy_Image != 7)
+		{
+			enemy_Image = 8;
+			std = 8; //セット処理
+		}
+		break;
+
+		/*ジャンプ*/
+	case 4: //4の画像
+		enemy_Image = 4;
+		std = 0; //セット処理
+		break;
+	case 9: //9の画像
+		enemy_Image = 9;
+		std = 0; //セット処理
+		break;
+
+		/*止まっている*/
+	//case 0: //0の画像
+	//	enemy_Image = 0;
+	//	std = 0; //セット処理
+	//	break;
+	//case 5: //5の画像
+	//	enemy_Image = 5;
+	//	std = 0; //セット処理
+	//	break;
+	default:
+		break;
+	}
+
+	/*走っているように画像変更*/
+	if (enemyCount % 10 == 0) //5フレーム毎の処理
+	{
+		/*右移動*/
+		if (dir == static_cast<int>(DIRECTION::RIGHT)) {
+			if (std == 1 && 2 > enemy_Image) // 画像 1
+			{
+				enemy_Image = 2;
+				std = 2;
+			}
+			else if (std == 2 && 3 > enemy_Image) // 画像 2
+			{
+				enemy_Image = 3;
+				std = 3;
+			}
+			else if (std == 3 && 2 < enemy_Image) //画像 3
+			{
+				enemy_Image = 2;
+				std = 4;
+			}
+			else if (std == 4 && 1 < enemy_Image) // 画像 2
+			{
+				enemy_Image = 1;
+				std = 1;
+			}
+			enemyCount = 0;
+		}
+		/*左移動*/
+		if (dir == static_cast<int>(DIRECTION::LEFT)) {
+			if (std == 6 && 7 > enemy_Image) // 画像 6
+			{
+				enemy_Image = 7;
+				std = 7;
+			}
+			else if (std == 7 && 8 > enemy_Image) // 画像 7
+			{
+				enemy_Image = 8;
+				std = 8;
+			}
+			else if (std == 8 && 7 < enemy_Image) // 画像 8
+			{
+				enemy_Image = 7;
+				std = 9;
+			}
+			else if (std == 9 && 6 < enemy_Image) // 画像 7
+			{
+				enemy_Image = 6;
+				std = 6;
+			}
+			enemyCount = 0;
+		}
+	}
+	//enemy_Image = enemy_Image + dir * 4;
+	//
+}
+
 
 //じゃん撃生成・更新
 void Enemy_10::Update_Jangeki()
@@ -209,8 +418,15 @@ void Enemy_10::Update_Jangeki()
 		/*********************** ↓↓ 生成( 追跡弾 ) ↓↓ ***********************/
 
 		//            生成速度
-		if (frame_count % 50 == 0) obj_jangeki[jan_count] = new Jangeki_Homing(x, y, radius, speed, type); //追跡弾 
+		//if (frame_count % 70 == 0) obj_jangeki[jan_count] = new Jangeki_Homing(x, y, radius, speed, type); //追跡弾 
 
+		/************************************************************************/
+
+		/*********************** ↓↓ 生成( プレイヤー向き通常弾 ) ↓↓ ***********************/
+
+		//            生成速度
+		if (frame_count % 70 == 0) obj_jangeki[jan_count] = new Jangeki_Coming(x, y, radius, speed, type, player_x, player_y);
+		
 		/************************************************************************/
 	}
 }
