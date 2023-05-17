5fc3cfd15bf38e93be58a93fa37d8958e37cedbf
Biggest Helper <107020759+Biggest-Helper@users.noreply.github.com>
2023-05-17 11:27:58 +0900

    ステージ6_瞬間移動接触処理の出現位置変更

--- Rock-Paper-Scissors/Enemy_06.cpp
+++ Rock-Paper-Scissors/Enemy_06.cpp
@@ -108,7 +108,7 @@ void Enemy_06::Update()
 		}
 
 		//500フレームごとに瞬間移動接触フラグをtrueにする
-		if (TeleportTime == 500 && TeleportFlg == false && (player_x - 250) > 50 && (player_x + 250) < 1230 && speed != 8.0f)
+		if (TeleportTime == 500 && TeleportFlg == false && (player_x - 230) > 50 && (player_x + 230) < 1230 && speed != 8.0f && hp > 0)
 		{
 			TeleportFlg = true;
 			if (attack_pattern != 0 && hp <= 70)
@@ -887,19 +887,19 @@ void Enemy_06::SpeedUp()
 //特殊行動2   プレイヤーの後方側に移動し、接近してくる
 void Enemy_06::Teleportation()
 {
-	if (TeleportInit == true && smokeFlg == false)
+	if (TeleportInit == true && smokeFlg == false && hp > 0)
 	{
 		if (player_dir == 0)
 		{
 			dir = -1;
-			x = player_x + 250;
+			x = player_x + 230;
 			y = 650.0f;
 		}
 
 		if (player_dir == 1)
 		{
 			dir = 1;
-			x = player_x - 250;
+			x = player_x - 230;
 			y = 650.0f;
 		}
 
